633d4751c78c56985ba7be5b6bbd5ababc225f54
robac <robert@poch.cz>
2022-01-12 09:03:52 +0100

    First functional try.

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from tools.tools import *
+import argparse
+import pprint
+from arubaswos.loginOs import *
 
-INPUT_MAC_FILENAME = 'data\\macs.txt'
-INPUT_PORT_FILENAME = 'data\\ports.txt'
-INPUT_ARP_FILENAME = 'data\\arps.txt'
 MAC_IGNORE_TRESHOLD = 5
 
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Aruba SWOS REST tool.')
+    parser.add_argument("-i", "--ip", action="store", dest="ip", required=True)
+    parser.add_argument("-p", "--password", action="store", dest="password", required=True)
+    parser.add_argument("-u", "--username", action="store", dest="username", required=True)
+    parser.add_argument('--ssl', dest='ssl', action='store_true')
+    parser.add_argument('--no-ssl', dest='ssl', action='store_false')
+    parser.set_defaults(ssl=False)
 
-def read_macs_input(ports, arps, devPatterns):
+    arguments = parser.parse_args()
+    return arguments
+
+def conn_data(arguments):
+    data = {}
+    data['password'] = arguments.password
+    data['user'] = arguments.username
+    data['ip'] = arguments.ip
+    data['ssl'] = arguments.ssl
+    return data
+
+
+
+
+"""
+def read_macs_input(ports, arps, devPatterns, data):
     input = open(INPUT_MAC_FILENAME, 'r')
     lines = input.readlines()
 
@@ -19,8 +41,8 @@ def read_macs_input(ports, arps, devPatterns):
         else:
             print("Unknown port: {}".format(mac.port))
     return
-
-
+"""
+"""
 def read_arps_input(devPatterns):
     input = open(INPUT_ARP_FILENAME, 'r')
     lines = input.readlines()
@@ -31,19 +53,17 @@ def read_arps_input(devPatterns):
         if (state in ['VLD', 'STS']):
             arps[mac] = ip
     return arps
-
-
+"""
+"""
 def read_ports_input(devPatterns):
-    input = open(INPUT_PORT_FILENAME, 'r')
-    lines = input.readlines()
     ports = {}
 
     for line in lines:
         port = PortInfo(line, devPatterns.get_pattern_compiled('arubaswos_farmtec_showint'))
         ports[port.port] = port
     return ports
-
-
+"""
+"""
 def print_ports(ports):
     for id, port in ports.items():
         if port.status == "Up":
@@ -70,5 +90,23 @@ def main():
     read_macs_input(ports, arps, devPatterns)
     print_ports(ports)
 
+"""
+def main():
+    arguments = get_arguments()
+    data = conn_data(arguments)
+    login_os(data)
+
+    try:
+        response = send_get_request(data, 'ports', 'port_element')
+        #for item in response:
+        #    print(item['mac_address'])
+        pprint.pprint(response)
+    except:
+        pass
+
+    logout(data)
+
 
 main()
+
+
