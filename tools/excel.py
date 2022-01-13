83e5af3b455488780d773973b77f5447ec283493
robac <robert@poch.cz>
2022-01-13 14:05:03 +0100

    Styling excel output

--- tools/excel.py
+++ tools/excel.py
@@ -1,6 +1,8 @@
 import openpyxl
 from openpyxl.styles import Font
+from openpyxl.styles import PatternFill
 
+BACKGROUND_COLOR = "E1DFDF"
 
 def export_excel(filename, ports):
     wb = openpyxl.Workbook()
@@ -9,7 +11,7 @@ def export_excel(filename, ports):
     sheet.column_dimensions['A'].width = 5
     sheet.column_dimensions['B'].width = 17
     sheet.column_dimensions['C'].width = 17
-    sheet.column_dimensions['D'].width = 17
+    sheet.column_dimensions['D'].width = 50
 
     crow = 1
     for p in ports.values():
@@ -19,11 +21,19 @@ def export_excel(filename, ports):
             c.font = Font(bold=True, color='FFFF0000')
         else:
             c.font = Font(bold=True)
+        c.fill = PatternFill(start_color=BACKGROUND_COLOR, end_color=BACKGROUND_COLOR, fill_type="solid")
+
+        c = sheet.cell(row=crow, column=2)
+        c.value = p.name
+        c.fill = PatternFill(start_color=BACKGROUND_COLOR, end_color=BACKGROUND_COLOR, fill_type="solid")
 
         c = sheet.cell(row=crow, column=3)
-        c.value = p.name
-        c = sheet.cell(row=crow, column=3)
         c.value = "{} MACs".format(len(p.macs))
+        c.fill = PatternFill(start_color=BACKGROUND_COLOR, end_color=BACKGROUND_COLOR, fill_type="solid")
+
+        c = sheet.cell(row=crow, column=4)
+        c.fill = PatternFill(start_color=BACKGROUND_COLOR, end_color=BACKGROUND_COLOR, fill_type="solid")
+
         crow += 1
         for mac in p.macs:
             c = sheet.cell(row=crow, column=2)
